cebb166ec2638ff1d02a810880710904e6773800
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-09 23:28:00 -0800

    Bunches of work!

--- src/CayleyMengerDeterminant.jl
+++ src/CayleyMengerDeterminant.jl
@@ -1,8 +1,47 @@
 module CayleyMengerDeterminant
 using LinearAlgebra
 
-struct CayleyMengerDistanceMatrix{T, V <: AbstractVector{T}}
-    distances::V
+index_triangular_nodiag(ixA, ixB) = binomial(ixA - 1, 2) + ixB
+
+struct CayleyMengerDistanceMatrix{T,V<:AbstractVector{T}} <: AbstractMatrix{T}
+    simplexDimensions::Int
+    squareDistances::V
 end
+CayleyMengerDistanceMatrix(onlyPoint::Tuple{}) =
+    CayleyMengerDistanceMatrix(1, (Union{})[])
+function CayleyMengerDistanceMatrix(
+    firstPoint::NTuple{N,T},
+    otherPoints::Vararg{NTuple{N,T},N},
+) where {N,T}
+    points = (firstPoint, otherPoints...)
+
+    sqrDists = Array{T}(undef, binomial(N+1, 2))
+    for pointIxA in 1:(N+1)
+        for pointIxB in 1:(pointIxA-1)
+            pos = index_triangular_nodiag(pointIxA, pointIxB)
+            sqrDist = sum((points[pointIxA] .- points[pointIxB]).^2)
+            sqrDists[pos] = sqrDist
+        end
+    end
+
+    CayleyMengerDistanceMatrix(N+1, sqrDists)
+end
+
+Base.:(==)(A::CayleyMengerDistanceMatrix, B::CayleyMengerDistanceMatrix) =
+    (A.simplexDimensions == B.simplexDimensions) && all(A.squareDistances .== B.squareDistances)
+
+Base.isequal(A::CayleyMengerDistanceMatrix, B::CayleyMengerDistanceMatrix) =
+    (A.simplexDimensions == B.simplexDimensions) && all(Base.isequal.(A.squareDistances, B.squareDistances))
+
+function Base.hash(A::CayleyMengerDistanceMatrix, h::UInt)
+    nh = h
+    nh = Base.hash(A.simplexDimensions, nh)
+    nh = Base.hash(A.squareDistances, nh)
+    nh
+end
+
+Base.widen(A::CayleyMengerDistanceMatrix) = CayleyMengerDistanceMatrix(A.simplexDimensions, widen.(A.squareDistances))
+
+export CayleyMengerDistanceMatrix
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,27 @@ using CayleyMengerDeterminant
 using Test
 
 @testset "CayleyMengerDeterminant.jl" begin
-    # Write your tests here.
+    @testset "index_triangular_nodiag" begin
+        N = 100
+        v = zeros(Int, binomial(N, 2))
+        for ixA = 1:N
+            for ixB = 1:(ixA-1)
+                v[CayleyMengerDeterminant.index_triangular_nodiag(ixA, ixB)] += 1
+            end
+        end
+        @test all(v .== 1)
+    end
+
+    @testset "CayleyMengerDistanceMatrix" begin
+        m0 = CayleyMengerDistanceMatrix(())
+        @test m0.simplexDimensions == 1
+        @test size(m0.squareDistances) == (0,)
+
+        
+        m1 = CayleyMengerDistanceMatrix((0,),(1,))
+        @test m1.simplexDimensions == 2
+        @test size(m1.squareDistances) == (1,)
+        @test m1.squareDistances[1] == 1
+        @test m1 == CayleyMengerDistanceMatrix((1,),(2,))
+    end
 end
